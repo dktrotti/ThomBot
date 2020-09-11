dbce210653abb37d470bd44b97dbea6813e58898
Dominic Trottier <domtrottier@outlook.com>
2020-09-10 19:44:32 -0700

    Initial commit

---
+++ ThomBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThomBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
